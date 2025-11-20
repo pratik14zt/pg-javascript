cc1994830c0972b93c726b52bd0d68fe4f0a0476
pratik14zt <pratikgurung.14zt@gmail.com>
2025-11-20 13:05:33 +0545

    fztpl accordion is in progress

---
+++ js/accordion1.js
@@ -0,0 +1,16 @@
+ const accordion = document.getElementsByClassName('contentBox');
+
+    for (let i = 0; i < accordion.length; i++) {
+        accordion[i].addEventListener('click', function () {
+
+            // 1️⃣ Close all other accordion items
+            for (let j = 0; j < accordion.length; j++) {
+                if (j !== i) {
+                    accordion[j].classList.remove('active');
+                }
+            }
+
+            // 2️⃣ Toggle the clicked one
+            this.classList.toggle('active');
+        });
+    }
