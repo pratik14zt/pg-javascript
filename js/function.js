f04b58d4c2e8f06526166ae95d5f04beb27703e1
pratik14zt <pratikgurung.14zt@gmail.com>
2025-11-27 12:29:11 +0545

    arrow functions practices

---
+++ js/function.js
@@ -0,0 +1,12 @@
+function pratik(){
+    console.log("Welcome to Js Learning");
+     console.log("Pratik is Learning JS");
+}
+
+pratik();
+
+//arrow function
+ 
+const pratik = (a,b) =>{
+    console.log(a+b);
+}
