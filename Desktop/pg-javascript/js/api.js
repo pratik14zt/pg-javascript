ed254f2ab5d7eca7e0c2751f14abeaabfa2a0a74
pratik14zt <pratikgurung.14zt@gmail.com>
2026-01-22 17:11:10 +0545

    fetch api and showcasing the html

--- Desktop/pg-javascript/js/api.js
+++ Desktop/pg-javascript/js/api.js
@@ -1,7 +1,13 @@
 const URL = "https://cat-fact.herokuapp.com/facts";
+const factVideo = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
 
 const getVideo = async () => {
     console.log("Fetching video data...");
     let response = await fetch(URL); 
     console.log("Response received.", response); //JSON Format
-}
+    let data = await response.json();
+    factVideo.innerText = data[2].text;
+};
+
+btn.addEventListener("click", getVideo);
